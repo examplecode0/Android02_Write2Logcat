955ead8729d54d76ca1e59bb9ad8c0b4e36293bc
Bharat <bharat226g@jessie>
2017-01-02 04:34:24 +0530

    Writing to Logcat

--- app/src/main/java/in/example/android02_write2logcat/MainActivity.java
+++ app/src/main/java/in/example/android02_write2logcat/MainActivity.java
@@ -2,6 +2,7 @@ package in.example.android02_write2logcat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        System.out.println("Writing to Logcat using System.out.println");
+        Log.i("SomeTag", "Writing to Logcat I");
     }
 }
